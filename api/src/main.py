89adf0e83f96f13975a31d877ea7b7b3326c4edd
akihironemoto <akihiro.nemoto.06@gmail.com>
2020-06-09 11:16:50 +0900

    add model & router

--- api/src/main.py
+++ api/src/main.py
@@ -1,8 +1,31 @@
 from fastapi import FastAPI
+from pets.routes import pets_router
+from config import config
 
 app = FastAPI()
 
-@app.get('/pets/')
-async def get_all_pets():
-    pets = [{'name': 'cat'}, {'name': 'dog'}]
-    return pets
+
+app.include_router(
+    pets_router,
+    prefix="/pets",
+    tags=["pets"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@app.on_event("startup")
+async def app_startup():
+    """
+    Do tasks related to app initialization.
+    """
+    # This if fact does nothing its just an example.
+    config.load_config()
+
+
+@app.on_event("shutdown")
+async def app_shutdown():
+    """
+    Do tasks related to app termination.
+    """
+    # This does finish the DB driver connection.
+    config.close_db_client()
